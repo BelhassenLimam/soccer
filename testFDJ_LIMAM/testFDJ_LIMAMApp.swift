cbf6711de46654389db245c8c8d406830a9fc989
Belhassen Limam <belhassen.limam-extern@renault.com>
2023-12-31 14:05:12 +0100

    Initial Commit

---
+++ testFDJ_LIMAM/testFDJ_LIMAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  testFDJ_LIMAMApp.swift
+//  testFDJ_LIMAM
+//
+//  Created by Belhassen LIMAM on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct testFDJ_LIMAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testFDJ_LIMAMUITests/testFDJ_LIMAMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  testFDJ_LIMAMUITestsLaunchTests.swift
+//  testFDJ_LIMAMUITests
+//
+//  Created by Belhassen LIMAM on 31/12/2023.
+//
+
+import XCTest
+
+final class testFDJ_LIMAMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
